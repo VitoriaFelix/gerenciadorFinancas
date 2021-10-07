2a6192229b037ba625c1a23bd75de1d4de708d22
VitoriaFelix <vitoriafelix22@hotmail.com>
2021-10-07 15:47:13 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,11 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 6), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 35 },
+    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel Apt', value: 2300 },
+    { date: new Date(2021, 10, 18), category: 'salary', title: 'Salário', value: 4500 },
+    { date: new Date(2021, 8, 20), category: 'lazer', title: 'Lazer', value: 250 },
+    { date: new Date(2021, 8, 25), category: 'supermarket', title: 'Supermecado', value: 500 },
+    { date: new Date(2021, 8, 30), category: 'salary', title: 'Salário', value: 3500 }
+];
